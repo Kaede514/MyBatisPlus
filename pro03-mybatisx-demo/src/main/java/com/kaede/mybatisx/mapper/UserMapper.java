8ae5c96c746d787c1c33de0e2d7f564f40cf111b
kaede <xx@qq.com>
2022-11-20 18:08:27 +0800

    commit

--- pro03-mybatisx-demo/src/main/java/com/kaede/mybatisx/mapper/UserMapper.java
+++ pro03-mybatisx-demo/src/main/java/com/kaede/mybatisx/mapper/UserMapper.java
@@ -1,4 +1,6 @@
 package com.kaede.mybatisx.mapper;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 import com.kaede.mybatisx.pojo.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
@@ -10,6 +12,16 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 * @Entity com.kaede.mybatisx.pojo.User
 */
 public interface UserMapper extends BaseMapper<User> {
+
+    int insertSelective(User user);
+
+    int deleteByUidAndUserName(@Param("uid") Long uid, @Param("userName") String userName);
+
+    int updateAgeAndSexByUid(@Param("age") Integer age, @Param("sex") Integer sex, @Param("uid") Long uid);
+
+    List<User> selectAgeAndSexByAgeBetween(@Param("beginAge") Integer beginAge, @Param("endAge") Integer endAge);
+
+    List<User> selectAllOrderByAgeDesc();
 
 }
 
